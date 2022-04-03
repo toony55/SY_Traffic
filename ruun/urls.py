acdac83c96a556dc648e841be78008c36f74c41a
tony <gaafarkhdr676@gmail.com>
2022-04-03 19:24:22 +0300

    heroku

--- ruun/urls.py
+++ ruun/urls.py
@@ -1,7 +1,10 @@
 
-from django.urls import include, path
+from django.urls import include, path,re_path
 from rest_framework import routers
 from .views import *
+from django.views.static import serve
+from django.conf import settings
+
 
 
 
@@ -17,5 +20,7 @@ router.register(r'bal',BalanceViewSet)
 urlpatterns = [
 
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
--- traffic/urls.py
+++ traffic/urls.py
@@ -1,13 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from django.views.static import serve
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("ruun.urls")),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 
 ]
 admin.site.site_header='SY Traffic Managment'
