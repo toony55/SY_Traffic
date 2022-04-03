cf61f07ab41b261fcfe9241353f24ab4542bc2f2
tony <gaafarkhdr676@gmail.com>
2022-04-03 22:26:57 +0300

    herokuuu

--- ruun/urls.py
+++ ruun/urls.py
@@ -21,6 +21,5 @@ urlpatterns = [
 
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
-    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 ]
