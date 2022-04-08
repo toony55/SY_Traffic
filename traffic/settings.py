d37c29fd46e97346053b302baad7817f5fb6bbf7
tony <gaafarkhdr676@gmail.com>
2022-04-08 11:03:35 +0300

    pwwd

--- traffic/settings.py
+++ traffic/settings.py
@@ -124,7 +124,6 @@ CORS_ALLOW_ALL_ORIGINS= True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
