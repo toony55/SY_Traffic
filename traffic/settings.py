d97c77f8ffa03292f6985dfa43cd2905d8ad6da5
tony <gaafarkhdr676@gmail.com>
2022-04-12 09:48:01 +0300

    pwwd

--- traffic/settings.py
+++ traffic/settings.py
@@ -130,3 +130,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# ie if Heroku server
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
