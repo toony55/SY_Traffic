18a86e78b86e61ee0a3f047a4924a9a5438991b5
tony <gaafarkhdr676@gmail.com>
2022-07-12 18:13:02 +0300

    Finished

--- traffic/settings.py
+++ traffic/settings.py
@@ -124,7 +124,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+WHITENOISE_USE_FINDERS = True
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
